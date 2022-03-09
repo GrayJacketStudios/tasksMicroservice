a7c7b36f8fd824f0ed39bd9c6bab9c485e311ab5
Sebastian Cristi <grayjacketstudios@gmail.com>
2022-03-08 22:50:19 -0300

    Created a common middleware to use in every request, without polluting the functions

--- src/handlers/getTask.js
+++ src/handlers/getTask.js
@@ -1,9 +1,6 @@
 import AWS from 'aws-sdk';
 
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const headers = {
@@ -40,7 +37,4 @@ const GetTask = async (event) => {
   };
 };
 
-export const handler = middy(GetTask)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(GetTask);
--- src/handlers/getTasks.js
+++ src/handlers/getTasks.js
@@ -1,9 +1,6 @@
 import AWS from 'aws-sdk';
 
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const headers = {
@@ -32,7 +29,4 @@ const GetTasks = async () => {
   };
 };
 
-export const handler = middy(GetTasks)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(GetTasks);
